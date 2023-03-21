0933df04ea34922e07f43f5bc52bf5306ddc227b
bzzdenchik <83644351+bzzdenchik@users.noreply.github.com>
2023-03-21 18:50:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,41 +6,41 @@ from matplotlib.patches import Rectangle
 
 
 def func(x):
-    f = 3*x - 2*x**2
+    f = 3*x - 2*x**2 #функция
     return f
 
-a = -1
-b = 2
-n = 100
-equipment = "random" #right/left/middle/random
+a = -1 #начало отрезка
+b = 2 #конец отрезка
+n = 100 #число разбиений
+equipment = "random" #right/left/middle/random (оснащение)
 
 segment = np.arange(a, b, (b-a)/n)
-segment = list(segment)
+segment = list(segment)             #отрезок интегрирования
 if float(b) not in segment:
     segment.append(float(b))
 
-x = []
+x = []#оснащение
 if equipment == "right":
     x = [segment[i] for i in range(1, len(segment))]
 elif equipment == "left":
     x = [segment[i] for i in range(len(segment)-1)]
 elif equipment == "middle":
-    for i in range(len(segment)-1):
+    for i in range(len(segment)-1):                                     
         x.append(segment[i]+((segment[i+1]-segment[i])/2))
 elif equipment == "random":
     for i in range(len(segment)-1):
         x.append(random.uniform(segment[i], segment[i+1]))
 
-sum = 0
+sum = 0#сумма
 y = [func(i) for i in x]
 fig,ax = plt.subplots()
-ax.plot(x,y)
+ax.plot(x,y) #выводим функцию
 
 for i in range(len(x)):
-    sum += y[i]*((b-a)/n)
-    ax.add_patch(Rectangle((x[i], 0), ((b-a)/n), y[i],edgecolor='black',facecolor='None', lw = 1))
+    sum += y[i]*((b-a)/n)#считаем сумму
+    ax.add_patch(Rectangle((x[i], 0), ((b-a)/n), y[i],edgecolor='black',facecolor='None', lw = 1)) #выводим нужный прямоугольник на экран
 
 
-print(sum)
+print(sum)#печатаем значение суммы
 
 plt.show()
